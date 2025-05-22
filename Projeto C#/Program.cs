f6d8e2e45170b3360e60361844471901f7a00367
Felipe Lundgren <felipec5l2@hotmail.com>
2025-05-22 15:57:43 -0300

    Concluindo Projeto

--- Projeto C#/Program.cs
+++ Projeto C#/Program.cs
@@ -1,7 +1,12 @@
 ﻿// Screen Sound
-string mensagemDeBoasVindas = "Boas Vindas ao Screen Sound";
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
 
-void ExibirMensagemDeBoasVindas()
+string mensagemDeBoasVindas = "Boas Vindas ao Screen Sound";
+Dictionary<string, List<int>> bandas = new Dictionary<string, List<int>>();
+bandas.Add("linkin park", new List<int> { 10,6,8,7});
+bandas.Add("Stratovarios",new List<int>());
+void ExibirLogo()
 {
     Console.WriteLine(@"
 ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
@@ -18,6 +23,7 @@ void ExibirMensagemDeBoasVindas()
 
 void ExibirOpcoesDoMenu()
 {
+    ExibirLogo();
     Console.WriteLine("\nDigite 1 para registrar uma banda");
     Console.WriteLine("Digite 2 para mostrar todas as bandas");
     Console.WriteLine("Digite 3 para avaliar uma banda");
@@ -30,13 +36,13 @@ void ExibirOpcoesDoMenu()
     
     switch(opcaoEscolhidaNumerica)
     {
-        case 1: Console.WriteLine("Voce escolheu a opcao: " +opcaoEscolhidaNumerica);
+        case 1: RegistrarBanda();
             break;
-        case 2: Console.WriteLine("Voce escolheu a opcao: " + opcaoEscolhidaNumerica);
+        case 2:ExibirBandas();
             break;
-        case 3: Console.WriteLine("Voce escolheu a opcao: " + opcaoEscolhidaNumerica);
+        case 3: AvaliarUmaBanda();
             break;
-        case 4: Console.WriteLine("Voce escolheu a opcao: " + opcaoEscolhidaNumerica);
+        case 4: ExibirNotaDeBanda();
             break;
         case 0: Console.WriteLine("Voce escolheu a opcao: " + opcaoEscolhidaNumerica + " Fechando o programa");
             break;
@@ -45,5 +51,112 @@ void ExibirOpcoesDoMenu()
     }
 }
 
-ExibirMensagemDeBoasVindas();
+void RegistrarBanda()
+{
+    Console.Clear();
+    ExibirTituloDaOpcao("Registro das bandas");
+    Console.WriteLine("Digite o nome da banda que deseja registrar: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    bandas.Add(nomeDaBanda, new List<int>());
+    Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+    Thread.Sleep(2500);
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+}
+
+void ExibirBandas()
+{
+    Console.Clear();
+    ExibirTituloDaOpcao("Exibir bandas");
+    
+    foreach(string banda in bandas.Keys)
+    {  
+        Console.WriteLine($"Banda: {banda}");
+    }
+    Console.WriteLine("\nDigite qualquer tecla para voltar ao menu principal");
+    Console.ReadKey();
+    Console.Clear();
+        ExibirOpcoesDoMenu();
+}
+
+void AvaliarUmaBanda()
+{
+    Console.Clear() ;
+    ExibirTituloDaOpcao("Avaliar banda");
+    Console.Write("Qual banda deseja avaliar: ");
+    string bandaEscolhida = Console.ReadLine()!;
+    if (bandas.ContainsKey(bandaEscolhida))
+    {
+        Console.Write($"Qual a note que {bandaEscolhida} merece: ");
+        int nota = int.Parse(Console.ReadLine()!);
+        bandas[bandaEscolhida].Add(nota);
+        Console.WriteLine($"A nota {nota} foi registada com sucesso");
+        Thread.Sleep(3000);
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    }
+    else 
+    {
+        Console.WriteLine($"\nA banda {bandaEscolhida} nao foi encontrada. ");
+        Console.WriteLine("Digite uma tecla para voltar ao menu principal. ");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    }
+
+}
+
+void ExibirNotaDeBanda()
+{
+    Console.Clear();
+    ExibirTituloDaOpcao("Exibir nota da banda");
+    Console.Write("Qual banda deseja verificar: ");
+    string bandaEscolhida = Console.ReadLine()!;
+    
+    if (bandas.ContainsKey(bandaEscolhida))
+    {
+        if (bandas[bandaEscolhida].Count > 0)
+        {
+
+            Console.WriteLine($"\nA media da banda {bandaEscolhida} é: {bandas[bandaEscolhida].Average()}");
+            Console.WriteLine("Digite uma tecla para voltar ao menu principal. ");
+            Console.ReadKey();
+            Console.Clear();
+            ExibirOpcoesDoMenu();
+
+            Thread.Sleep(3000);
+            Console.Clear();
+            ExibirOpcoesDoMenu();
+
+        }
+        else
+        {
+            Console.WriteLine($"\nA banda {bandaEscolhida} nao possui nenhuma avaliacao. ");
+            Thread.Sleep(3000);
+            Console.Clear();
+            ExibirOpcoesDoMenu();
+        }
+    }   
+
+    else
+    {
+        Console.WriteLine($"\nA banda {bandaEscolhida} nao foi encontrada. ");
+        Console.WriteLine("Digite uma tecla para voltar ao menu principal. ");
+        Thread.Sleep(3000);
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    }
+}
+
+void ExibirTituloDaOpcao(string titulo)
+    { 
+    int quantidadeDeLetras = titulo.Length;
+    string astericos = string.Empty.PadLeft(quantidadeDeLetras, '*');
+    Console.WriteLine(astericos );
+    Console.WriteLine(titulo);
+    Console.WriteLine(astericos + "\n");
+    }
+
+
+
 ExibirOpcoesDoMenu();
